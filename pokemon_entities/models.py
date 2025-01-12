b01275b5f9aa97639c4d02b8831469ab907760c9
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-01-12 22:32:04 +0400

    delete default value from CharFields title_en, title_jp

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,9 +4,9 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200,
                                 verbose_name='Название (русское)')
-    title_en = models.CharField(max_length=200, default='', blank=True,
+    title_en = models.CharField(max_length=200, blank=True,
                                 verbose_name='Название (английское)')
-    title_jp = models.CharField(max_length=200, default='', blank=True,
+    title_jp = models.CharField(max_length=200, blank=True,
                                 verbose_name='Название (японское)')
     image = models.ImageField(null=True, blank=True,
                               verbose_name='Изображение')
